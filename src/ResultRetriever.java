65fccfd9d62e43b5b9db2627bd05195a2240d71d
Stefan <37920295+imbactbulletz@users.noreply.github.com>
2019-04-07 01:43:18 +0200

    Implemented summary for File Scanner.

--- src/ResultRetriever.java
+++ src/ResultRetriever.java
@@ -1,9 +1,10 @@
 import job.FileJob;
 
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 public class ResultRetriever extends Thread {
 
@@ -32,14 +33,39 @@ public class ResultRetriever extends Thread {
         String name = splitQuery[1];
 
         if(scanType.equals("file")) {
+
+            if(name.equals("summary")) {
+                List<Future<Map>> jobFutures = new ArrayList<>(fileJobsSummaryCache.values());
+
+                for(Future jobFuture : jobFutures) {
+                    if(!jobFuture.isDone()) {
+                        try {
+                            jobFuture.get();
+                        } catch (InterruptedException | ExecutionException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                Map<String, Map<String, Integer>> summaryMap = new HashMap<>();
+
+                fileJobsSummaryCache.forEach((k,v) -> {
+                    try {
+                        summaryMap.put(k, v.get());
+                    } catch (InterruptedException | ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+                return summaryMap.toString();
+            }
+
             Future<Map> jobFuture = fileJobsSummaryCache.get(name);
 
             if(jobFuture != null) {
                 try {
                     return jobFuture.get().toString();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
+                } catch (InterruptedException | ExecutionException e) {
                     e.printStackTrace();
                 }
             } else {
@@ -57,6 +83,28 @@ public class ResultRetriever extends Thread {
         String name = splitQuery[1];
 
         if(scanType.equals("file")) {
+            if(name.equals("summary")) {
+                List<Future<Map>> jobFutures = new ArrayList<>(fileJobsSummaryCache.values());
+
+                for(Future jobFuture : jobFutures) {
+                    if(!jobFuture.isDone()) {
+                       return "File Summary is not ready yet.";
+                    }
+                }
+
+                Map<String, Map<String, Integer>> summaryMap = new HashMap<>();
+
+                fileJobsSummaryCache.forEach((k,v) -> {
+                    try {
+                        summaryMap.put(k, v.get());
+                    } catch (InterruptedException | ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+                return summaryMap.toString();
+            }
+
             Future<Map> jobFuture = fileJobsSummaryCache.get(name);
 
             if(jobFuture != null) {
@@ -64,9 +112,7 @@ public class ResultRetriever extends Thread {
                 if(jobFuture.isDone()) {
                     try {
                         return jobFuture.get().toString();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (ExecutionException e) {
+                    } catch (InterruptedException | ExecutionException e) {
                         e.printStackTrace();
                     }
                 } else {
@@ -79,6 +125,7 @@ public class ResultRetriever extends Thread {
 
         return "No job under such query exists.";
     }
+
 
     public void clearFileSummary() {
         fileJobsSummaryCache = new ConcurrentHashMap<>();
